2be1345eea09ce928f8c577db276782c2568b37b
phuchh <koolkido1412@gmail.com>
2019-04-17 08:53:54 +0700

    create base class

--- app/src/main/java/phuchh/sunasterisk/moviedb_41/ui/main/MainActivity.kt
+++ app/src/main/java/phuchh/sunasterisk/moviedb_41/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package phuchh.sunasterisk.moviedb_41
+package phuchh.sunasterisk.moviedb_41.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import phuchh.sunasterisk.moviedb_41.R
 
 class MainActivity : AppCompatActivity() {
 
